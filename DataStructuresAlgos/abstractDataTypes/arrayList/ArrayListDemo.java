37a8e188ceeeb88510cd0315e37cb5ca3f975b5b
vsaxena12 <vsaxena1@binghamton.edu>
2020-10-11 20:04:11 -0400

    ArrayList

---
+++ DataStructuresAlgos/abstractDataTypes/arrayList/ArrayListDemo.java
@@ -0,0 +1,61 @@
+package arrayList;
+import java.util.ArrayList;
+
+class ArrayListTest
+{
+	public void testArrayList()
+	{
+		ArrayList<String> str = new ArrayList<String>();
+		str.add("1");
+		str.add("2");
+		str.add("3");
+		str.add("4");
+		str.add("5");
+		str.add("6");
+		
+		System.out.print(str);
+		str.remove("3"); //Removing Object
+		System.out.print(str);
+		
+	}
+}
+
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Legacy Implementations - 
+		 * Generally we avoid these DS because they are
+		 * Synchronized 
+		 * Vector - ArrayList 
+		 * Hash Tables - HashMap 
+		 * Stack - ArrayDequeue
+		 */
+		
+		// ---------Methods---------
+		//add(index, element) - Element Shift right by one position
+		//remove(index) - Element Shift left by one position
+		//Contains()
+		//indexOf()
+		//equals()
+		//removeAll()
+		//retainAll()
+		//size()
+		//isEMpty()
+		//set() & get()
+		//iterator() and listiterator()
+		
+		ArrayListTest alt = new ArrayListTest();
+		alt.testArrayList();
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
