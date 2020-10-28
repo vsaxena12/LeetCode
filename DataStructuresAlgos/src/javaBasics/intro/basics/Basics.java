60d71c38c6aa5fd8e6d91539659d0113922ab4b0
vsaxena12 <vsaxena1@binghamton.edu>
2020-10-28 15:31:45 -0400

    New Line

---
+++ DataStructuresAlgos/src/javaBasics/intro/basics/Basics.java
@@ -0,0 +1,8 @@
+package javaBasics.intro.basics;
+
+public class Basics {
+	public void print()
+	{
+		System.out.println("Hello World!!");
+	}
+}
---
+++ DataStructuresAlgos/src/javaBasics/intro/driver/Driver.java
@@ -0,0 +1,14 @@
+package javaBasics.intro.driver;
+
+import javaBasics.intro.basics.*;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Basics basics = new Basics();
+		basics.print();
+		
+	}
+
+}
