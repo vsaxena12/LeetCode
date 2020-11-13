f468a35af3d427a1583084da64ffd45c8a34b04f
vsaxena12 <vsaxena1@binghamton.edu>
2020-11-12 22:47:13 -0500

    Update LinkedList

--- DataStructuresAlgos/singlyLinkedList/src/linkedListConcepts/driver/Driver.java
+++ DataStructuresAlgos/singlyLinkedList/src/linkedListConcepts/driver/Driver.java
@@ -5,18 +5,25 @@ import linkedListConcepts.nodeLinkedList.LinkedList;
 
 public class Driver
 {
-	public static void main(String args[])
+	public static void main(String args[]) //Point of Execution
 	{
+		Node head = null;
+		System.out.println("Head: "+head);
+		
 		System.out.println("Starting LinkedList");
 		LinkedList linkedList = new LinkedList();
 		linkedList.addLinkedList(10);
-		linkedList.addLinkedList(20);
-		linkedList.addLinkedList(30);
 		linkedList.addLinkedList(40);
-		linkedList.addLinkedList(50);
+		linkedList.addLinkedList(30);
 		linkedList.addLinkedList(60);
-		linkedList.addLinkedList(70);
+		linkedList.addLinkedList(40);
 		linkedList.addLinkedList(80);
+		linkedList.addLinkedList(50);
+		linkedList.addLinkedList(20);
 		linkedList.printNode();
+
+		linkedList.countNodes(head);
+		linkedList.sumOfNodes(head);
+		linkedList.maxElement(head);
 	}
 }
--- DataStructuresAlgos/singlyLinkedList/src/linkedListConcepts/nodeLinkedList/LinkedList.java
+++ DataStructuresAlgos/singlyLinkedList/src/linkedListConcepts/nodeLinkedList/LinkedList.java
@@ -29,7 +29,7 @@ public class LinkedList
 			{
 				//System.out.print(newNode.data+" ");
 
-				newNode = newNode.next;
+				newNode = newNode.next; //use for traversing
 				//System.out.println("Node.data -> "+node.data+" Node.next -> "+node.next);
 				//System.out.println("head.data -> "+head.data+" head.next -> "+head.next);
 				
@@ -52,4 +52,74 @@ public class LinkedList
 		}
 		System.out.println();
 	}
+
+	//Count Number of Nodes
+	public void countNodes(Node countTemp)
+	{
+		countTemp = head;
+		int count = 0;
+		while(countTemp != null)
+		{
+			count++;
+			countTemp = countTemp.next;
+		}
+		System.out.println("Count Nodes: "+count);
+	}
+
+	//Sum of Nodes
+	public void sumOfNodes(Node addNodeTemp)
+	{
+		System.out.println("AddNodeTemp: "+addNodeTemp);
+		
+		addNodeTemp = head; //addNodeTemp is no longer null
+		System.out.println("AddNodeTemp: "+addNodeTemp.data+" -> "+addNodeTemp.next);
+
+		int sum = 0;
+		while(addNodeTemp != null)
+		{
+			sum = sum + addNodeTemp.data;
+			addNodeTemp = addNodeTemp.next;
+		}
+		System.out.println("Sums Nodes: "+sum);
+	}
+
+	//Maximum Element
+	public void maxElement(Node maxNode)
+	{
+		maxNode = head;
+		int maxValue = Integer.MIN_VALUE;
+		System.out.println("Default Max Value: "+maxValue);
+
+		while(maxNode != null)
+		{
+			if(maxNode.data >= maxValue)
+			{
+				maxValue = maxNode.data;
+				System.out.println("Max Element Traversal: "+maxValue);
+			}
+			maxNode = maxNode.next;
+		}
+
+		System.out.println("Max Element: "+maxValue);
+
+	}
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
