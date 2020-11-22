dc4397a9108928800179aa886b6bef59e07d9555
vsaxena12 <vsaxena1@binghamton.edu>
2020-11-21 20:26:05 -0500

    Test Recursion

---
+++ DataStructuresAlgos/recursion/src/recursion/driver/Driver.java
@@ -0,0 +1,17 @@
+package recursion.driver;
+
+
+public class Driver
+{
+	//static Node head; //It is a reference pointer
+	public static void main(String args[]) //Point of Execution
+	{
+		recursion();
+
+	}
+
+	public static void recursion()
+	{
+		System.out.println("Recursion ");
+	}
+}
