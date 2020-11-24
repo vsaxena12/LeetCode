89b551af0fd2fe11a939cb47340875847a4ee7ec
vsaxena12 <vsaxena1@binghamton.edu>
2020-11-24 07:38:48 -0500

    Starting Doubly Linkedlist

---
+++ DataStructuresAlgos/doublyLinkedList/src/linkedListConcepts/driver/Driver.java
@@ -0,0 +1,11 @@
+package linkedListConcepts.driver;
+
+//import linkedListConcepts.driver.Driver;
+
+public class Driver
+{
+	public static void main(String args[])
+	{
+		System.out.println("Starting LinkedList");
+	}
+}
---
+++ DataStructuresAlgos/doublyLinkedList/src/linkedListConcepts/singlyLinkedList/Node.java
@@ -0,0 +1,13 @@
+
+package linkedListConcepts.singlyLinkedList;
+
+class Node
+{
+	int data;
+	Node next;
+
+	Node(int data)
+	{
+		this.data = data;
+	}
+}
