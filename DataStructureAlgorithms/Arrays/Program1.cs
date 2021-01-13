0749466599b04df150b2b908ea87d2e626e36250
iVar <ivar@iVars-MacBook-Pro.local>
2021-01-12 20:27:29 -0500

    Test

---
+++ DataStructureAlgorithms/Arrays/Program1.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Program1
+{
+    class Test
+    {
+        public void test()
+        {
+            Console.WriteLine("LeetCode");
+        }
+    }
+
+    class Program1
+    {
+        public static void Main(string[] args)
+        {
+            Test t = new Test();
+            t.test();
+            
+        }
+    }
+}
