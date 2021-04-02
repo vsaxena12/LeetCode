7c0cca823e7f8d13cdbe7a9824d16abed5cb9a2c
vsaxena12 <vsaxena1@binghamton.edu>
2021-04-02 07:25:22 -0400

    AverageOfSubarrayOfSize

---
+++ Java/Data Structures and Algorithms/WindowSliding/src/WindowSliding/ProblemStatements/AverageOfSubarrayOfSize.java
@@ -0,0 +1,54 @@
+package WindowSliding.ProblemStatements;
+import java.util.Arrays;
+
+public class AverageOfSubarrayOfSize
+{
+	//Brute Force 
+	public static double[] findAverages(int k, int[] arr) 
+	{
+    	double[] result = new double[arr.length - k + 1];
+    	for(int i=0; i<arr.length-k+1; i++)
+    	{
+    		double sum = 0;
+    		for(int j=i; j<k+i; j++)
+    		{
+    			sum = sum+arr[j];
+    		}
+    		result[i] = sum/k;
+    	}
+    	return result;
+  	}
+
+  	//Optimized Sol
+  	public static double[] findAveragesOptimized(int k, int[] arr) 
+	{
+    	double[] result = new double[arr.length - k + 1];
+    	int windowStart = 0;
+    	double sum = 0;
+
+    	for(int windowEnd = 0; windowEnd<arr.length; windowEnd++)
+    	{
+    		sum = sum + arr[windowEnd];
+
+    		if(windowEnd >= k-1)
+    		{
+    			result[windowStart] = sum/k;
+    			sum = sum - arr[windowStart];
+    			windowStart++;
+
+    		}
+    	}
+
+
+    	return result;
+  	}
+
+  	public void result() 
+  	{
+    	double[] result = AverageOfSubarrayOfSize.findAverages(5, new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 });
+    	System.out.println("Averages of subarrays of size K: " + Arrays.toString(result));
+
+    	result = AverageOfSubarrayOfSize.findAveragesOptimized(5, new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 });
+    	System.out.println("Averages of subarrays of size K Optimized: " + Arrays.toString(result));
+  	}
+}
---
+++ Java/Data Structures and Algorithms/WindowSliding/src/WindowSliding/driver/Driver.java
@@ -0,0 +1,14 @@
+package WindowSliding.driver;
+import WindowSliding.ProblemStatements.AverageOfSubarrayOfSize;
+
+
+public class Driver
+{
+	public static void main(String args[])
+	{
+		System.out.println("Test");
+
+		AverageOfSubarrayOfSize result = new AverageOfSubarrayOfSize();
+		result.result();
+	}
+}
